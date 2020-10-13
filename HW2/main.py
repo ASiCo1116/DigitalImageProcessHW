0efea7a06fc2f4e2503bf8d890fbed7e932074a6
ASiCo1116 <b05611046@ntu.edu.tw>
2020-10-14 01:10:19 +0800

    [1014]

--- HW2/main.py
+++ HW2/main.py
@@ -2,9 +2,8 @@
 
 from sys import argv
 from os import getcwd
-from math import floor, ceil
 from cv2 import imread, split, merge
-from numpy import zeros, arange, float32, sum, array, histogram, cumsum, int, around
+from numpy import zeros, arange, float32, sum, array, histogram, cumsum, int, around, divmod, floor, ceil, reshape, any
 
 from PyQt5.QtCore import (QSize)
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QHBoxLayout, QWidget, QFileDialog)
@@ -28,37 +27,34 @@ def equalization(image):
     return new_image
 
 def bilinearInterpolation(image, ratio):
-
-    img_height, img_width = image.shape[:2]
-    height, width = int(img_height * (ratio/100) + 1), int(img_width * (ratio/100) + 1)
-
-    resized = zeros(shape = (height, width))
+    img_height, img_width = image.shape
+    height, width = int(img_height * (ratio / 100)), int(img_width * (ratio / 100))
+    image = image.ravel()
 
     x_ratio = float(img_width - 1) / (width - 1) if width > 1 else 0
     y_ratio = float(img_height - 1) / (height - 1) if height > 1 else 0
 
-    for i in range(height):
-        for j in range(width):
+    y, x = divmod(arange(height * width), width)
 
-            x_l, y_l = floor(x_ratio * j), floor(y_ratio * i)
-            x_h, y_h = ceil(x_ratio * j), ceil(y_ratio * i)
+    x_l = floor(x_ratio * x).astype('int')
+    y_l = floor(y_ratio * y).astype('int')
 
-            x_weight = (x_ratio * j) - x_l
-            y_weight = (y_ratio * i) - y_l
+    x_h = floor(x_ratio * x).astype('int')
+    y_h = floor(y_ratio * y).astype('int')
 
-            a = image[y_l, x_l]
-            b = image[y_l, x_h]
-            c = image[y_h, x_l]
-            d = image[y_h, x_h]
+    x_weight = (x_ratio * x) - x_l
+    y_weight = (y_ratio * y) - y_l
 
-            pixel = a * (1 - x_weight) * (1 - y_weight) + \
-                    b * x_weight * (1 - y_weight) + \
-                    c * y_weight * (1 - x_weight) + \
-                    d * x_weight * y_weight
+    a = image[y_l * img_width + x_l]
+    b = image[y_l * img_width + x_h]
+    c = image[y_h * img_width + x_l]
+    d = image[y_h * img_width + x_h]
 
-            resized[i][j] = pixel
-
-    return resized
+    resized = a * (1 - x_weight) * (1 - y_weight) + \
+                b * x_weight * (1 - y_weight) + \
+                c * y_weight * (1 - x_weight) + \
+                d * x_weight * y_weight
+    return resized.reshape(height, width)
 
 '''
 Main ui
@@ -69,11 +65,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.cwd = getcwd()
+        '''
+        For saving fig
+        '''
         self.raw.fig.canvas.mpl_connect('button_press_event', self.onclick)
-        
-        
+        self.gray1.fig.canvas.mpl_connect('button_press_event', self.saveImage)
+        self.gray1_hist.fig.canvas.mpl_connect('button_press_event', self.saveImage)
+        self.gray2.fig.canvas.mpl_connect('button_press_event', self.saveImage)
+        self.gray2_hist.fig.canvas.mpl_connect('button_press_event', self.saveImage)
+        self.subtract_hist.fig.canvas.mpl_connect('button_press_event', self.saveImage)
+
+        self.resize_gray.fig.canvas.mpl_connect('button_press_event', self.saveImage)
+        self.resize_hist.fig.canvas.mpl_connect('button_press_event', self.saveImage)
+        self.bright_contrast.fig.canvas.mpl_connect('button_press_event', self.saveImage)
+        self.binary.fig.canvas.mpl_connect('button_press_event', self.saveImage)
+
         self.resize_slider.setProperty('value', 100)
         self.resize_slider.setMinimum(10)
+        # self.resize_slider.setSingleStep(1)
         self.resize_slider.setMaximum(200)
 
         self.binary_slider.setProperty('value', 128)
@@ -94,6 +103,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.resize_slider.valueChanged.connect(self.resizeValueChanged)
         self.equal_btn.clicked.connect(self.equalizationClicked)
     
+    '''
+    Saving image when clicking the picture
+    '''
+    def saveImage(self, e):
+        imageName, _ = QFileDialog.getSaveFileName(self,  
+                                    "Save Image As",  
+                                    "",
+                                    "Image Files (*.png *.jpg)")  
+
+        if imageName == "":
+            return
+
+        if imageName:
+            e.canvas.figure.savefig(imageName)
+
     '''
     Read picture function
     '''
@@ -124,8 +148,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.gray1.draw()
         
         self.gray1_hist.axes.cla()
-        self.gray1_hist.axes.hist(self.gray1_img.flatten(), bins=arange(255) - .5)
-        # self.gray1_hist.axes.set_title('grayscale histogram', fontsize=12)
+        self.gray1_hist.axes.hist(self.gray1_img.flatten(), bins=arange(256))
         self.gray1_hist.axes.set_axis_off()
         self.gray1_hist.draw()
 
@@ -136,13 +159,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.gray2.draw()
         
         self.gray2_hist.axes.cla()
-        self.gray2_hist.axes.hist(self.gray2_img.flatten(), bins=arange(255) - .5)
-        # self.gray2_hist.axes.set_title('grayscale histogram', fontsize=12)
+        self.gray2_hist.axes.hist(self.gray2_img.flatten(), bins=arange(256))
         self.gray2_hist.axes.set_axis_off()
         self.gray2_hist.draw()
 
         self.subtract_hist.axes.cla()
-        self.subtract_hist.axes.hist((self.gray2_img - self.gray1_img).flatten(), bins = arange(255) - .5)
+        self.subtract_hist.axes.hist((self.gray2_img - self.gray1_img).flatten(), bins = arange(256))
         self.subtract_hist.axes.set_axis_off()
         self.subtract_hist.draw()
 
@@ -169,31 +191,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def binaryValueChanged(self, v):
         
-        # self.binary_img[self.gray1_img >= v] = 255
-        # self.binary_img[self.gray1_img <= v] = 0
-
-        # self.binary.axes.cla()
-        # self.binary.axes.imshow(self.binary_img, cmap='gray', vmin=0, vmax=255)
-        # self.binary.axes.set_axis_off()
-        # self.binary.draw()
-
+        self.binary_img[self.gray1_img >= v] = 255
+        self.binary_img[self.gray1_img <= v] = 0
         self.rePlot(self.binary_img, self.binary, self.resize_hist)
 
-
     def brightnessValueChanged(self, v):
-        # self.bright_contrast.axes.cla()
-        # self.bright_contrast.axes.imshow(self.brightness_contrast(v, self.contrast_slider.value()), cmap='gray', vmin=0, vmax=255)
-        # self.bright_contrast.axes.set_axis_off()
-        # self.bright_contrast.draw()
-
         self.rePlot(self.brightness_contrast(v, self.contrast_slider.value()), self.bright_contrast, self.resize_hist)
     
     def contrastValueChanged(self, v):  #v from -254 to 258
-        # self.bright_contrast.axes.cla()
-        # self.bright_contrast.axes.imshow(self.brightness_contrast(self.bright_slider.value(), v), cmap='gray', vmin=0, vmax=255)
-        # self.bright_contrast.axes.set_axis_off()
-        # self.bright_contrast.draw()
-
         self.rePlot(self.brightness_contrast(self.bright_slider.value(), v), self.bright_contrast, self.resize_hist)
     
     def brightness_contrast(self, b, c):
@@ -204,57 +209,37 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         factor = 259 * (c + 255) / (255 * (259 - c))
         new_image = (factor * (b_img - 128) + 128).copy()
 
-        new_image[new_image > 255] = 255
-        new_image[new_image < 0] = 0
+        new_image[new_image >= 255] = 255
+        new_image[new_image <= 0] = 0
         return new_image
 
     def resizeValueChanged(self, v):
-        # self.resize_gray.axes.cla()
-        # self.resize_gray.axes.imshow(bilinearInterpolation(self.resize_gray_img, v), cmap='gray', vmin=0, vmax=255)
-        # self.resize_gray.axes.set_axis_off()
-        # self.resize_gray.draw()
-
         self.rePlot(bilinearInterpolation(self.resize_gray_img, v), self.resize_gray, self.resize_hist)
     
     def equalizationClicked(self):
-        # self.bright_contrast.axes.cla()
-        # self.bright_contrast.axes.imshow(equalization(self.bright_contrast_img), cmap='gray', vmin = 0, vmax = 255)
-        # self.bright_contrast.axes.set_axis_off()
-        # self.bright_contrast.draw()
-        
         self.rePlot(equalization(self.bright_contrast_img), self.bright_contrast, self.resize_hist)
 
-    '''
-    NOT DONE YET
-    '''
-    def saveImage(self):
-        imageChoose, _ = QFileDialog.getSaveFileName(self,  
-                                    "Save image",  
-                                    self.title + '.png',
-                                    "Image Files (*.png, *.jpg)")  
-
-        if imageChoose == "":
-            return
 
     '''
-    Replotting histogram and processed image when value changes
+    Updating histogram and processed image when value changes
     '''
     def rePlot(self, img, processed_widget, hist_widget = None):
        
         processed_widget.axes.cla()
-        processed_widget.axes.imshow(img, cmap = 'gray', vmin = 0, vmax = 255)
+        processed_widget.axes.imshow(img, cmap='gray', vmin=0, vmax=255)
         processed_widget.axes.set_axis_off()
         processed_widget.draw()
         
         if hist_widget != None:
-            img = img.flatten()
+            img = img.astype(int).flatten()
             img[img >= 255] = 255
             img[img <= 0] = 0
             # print(img)
+            # hist_widget.fig.figsize=(1, 0.5)
             hist_widget.axes.cla()
-            hist_widget.axes.hist(img, bins=arange(256) - .5)
-            hist_widget.axes.set_axis_off()
-            # hist_widget.axes.set_xticks(ticks = list(range(0, 256, 5)), minor = True)
+            hist_widget.axes.hist(img, bins=arange(256))
+            hist_widget.axes.tick_params(labelsize=2.0)
+            # hist_widget.axes.set_xticks(ticks = list(range(0, 256, 5)), minor = False)
             hist_widget.draw()
 
     '''
