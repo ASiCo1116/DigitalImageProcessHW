ceb6e1db13323977222f5d53e035e742041b86d2
ASiCo1116 <b05611046@ntu.edu.tw>
2020-11-11 09:09:09 +0800

    [1111]

--- HW4/main.py
+++ HW4/main.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import cv2 as cv
+import numpy as np
+import math
 from time import time
 from sys import argv
 from os import getcwd
-from cv2 import imread, split, merge
+from cv2 import imread, split, merge, dft, idft
 from numpy import zeros, arange, float32, sum, array, histogram, cumsum, int, around, divmod, floor, ceil, reshape, any, dot, random, median, amax, amin, ones, log, angle
-from numpy.fft import fft2, ifft2, fftshift
+from numpy.fft import fft2, ifft2, fftshift, ifftshift
 from scipy.signal import convolve2d
 from scipy.ndimage import gaussian_filter, gaussian_laplace, sobel, laplace
 from PyQt5.QtCore import (QSize)
@@ -66,10 +69,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.angle.draw()
 
         #some bug in ifft
-        # self.ifft.axes.cla()
-        # self.ifft.axes.imshow(ifft, cmap='gray')
-        # self.ifft.axes.set_axis_off()
-        # self.ifft.draw()
+        self.ifft.axes.cla()
+        self.ifft.axes.imshow(ifft, cmap='gray')
+        self.ifft.axes.set_axis_off()
+        self.ifft.draw()
 
 
     
@@ -97,16 +100,41 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.filter.draw()
     
     def _problem1(self, img):
-        fft2_img = fft2(img).copy()
-        _min, _max = log(1 + abs(fft2_img.min())), log(1 + abs(fft2_img.max()))
-        new_img = 255 * (log(1 + abs(fft2_img)) - _min) / (_max - _min)
-        return new_img, angle(fft2_img), ifft2(new_img)
+        angle_img = fft2(img)
+        fft2_img = dft(img, flags=cv.DFT_COMPLEX_OUTPUT)
+        fft2_img_shift = fftshift(fft2_img)
+        mag = cv.magnitude(fft2_img_shift[:,:, 0], fft2_img_shift[:,:, 1])
+        _min, _max = log(1 + abs(mag.min())), log(1 + abs(mag.max()))
+        new_img = 255 * (log(1 + abs(mag)) - _min) / (_max - _min)
+
+        idft_img = idft(ifftshift(fft2_img_shift), flags=cv.DFT_SCALE)
+        new_img_ifft = cv.magnitude(idft_img[:,:, 0], idft_img[:,:, 1])
+        new_img_ifft[new_img_ifft > 255] = 255
+        new_img_ifft[new_img_ifft < 0] = 0
+
+        return new_img, angle(angle_img), new_img_ifft
 
     def filter_radio(self):
         btnId = self.filterButton.checkedId()  #-2 ~ -10
         
-        if btnId == -5:
-            self._ILP()
+        if btnId == -2:  #IHP
+            self._IP('high')
+        elif btnId == -3: #BWHP
+            self._BW('high')
+        elif btnId == -4: #GHP
+            self._GP('high')
+        elif btnId == -5: #ILP
+            self._IP('low')
+        elif btnId == -6: #BWLP
+            self._BW('low')
+        elif btnId == -7: #GLP
+            self._GP('low')
+        elif btnId == -8:
+            self._HOMO()
+        elif btnId == -9:
+            self._MB()
+        elif btnId == 10:
+            self._MBN()
 
         self.filter.axes.cla()
         self.filter.axes.imshow(self.filtered_img, cmap='gray')
@@ -114,46 +142,123 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.filter.draw()
 
     
-    def _ILP(self):
+    def _IP(self, mode):
         
+        if mode == 'low':
+
+            filter = zeros(shape=self.processed_img.shape)
+            for i in range(filter.shape[0]):
+                for j in range(filter.shape[1]):
+                    if ((i - filter.shape[0] / 2)** 2 + (j - filter.shape[1] / 2)** 2)** .5 <= self.cutoff_box.value():
+                        filter[i][j] = 1
+
+        if mode == 'high':
+
+            filter = ones(shape=self.processed_img.shape)
+            for i in range(filter.shape[0]):
+                for j in range(filter.shape[1]):
+                    if ((i - filter.shape[0] / 2)** 2 + (j - filter.shape[1] / 2)** 2)** .5 <= self.cutoff_box.value():
+                        filter[i][j] = 0
+
+        _dft = fftshift(dft(self.processed_img, flags=cv.DFT_COMPLEX_OUTPUT))
+        self.filtered_img = _dft.copy()
+        self.filtered_img[:,:, 0] = _dft[:,:, 0] * filter
+        self.filtered_img[:,:, 1] = _dft[:,:, 1] * filter
+        self.filtered_img = idft(ifftshift(self.filtered_img), flags=cv.DFT_SCALE)
+        self.filtered_img = cv.magnitude(self.filtered_img[:,:, 0], self.filtered_img[:,:, 1])
+        self.filtered_img[self.filtered_img > 255] = 255
+        self.filtered_img[self.filtered_img < 0] = 0
+    
+    def _BW(self, mode):
+        if mode == 'high':
+            filter = zeros(shape=self.processed_img.shape)
+            for i in range(filter.shape[0]):
+                for j in range(filter.shape[1]):
+                    d = ((i - filter.shape[0] / 2)** 2 + (j - filter.shape[1] / 2)** 2)** .5
+                    filter[i][j] = 1 / (1 + (self.cutoff_box.value() / d)**(2 * self.order_box.value()))
+                    
+        if mode == 'low':
+            filter = zeros(shape=self.processed_img.shape)
+            for i in range(filter.shape[0]):
+                for j in range(filter.shape[1]):
+                    d = ((i - filter.shape[0] / 2)** 2 + (j - filter.shape[1] / 2)** 2)** .5
+                    filter[i][j] = 1 / (1 + (d / self.cutoff_box.value())**(2 * self.order_box.value()))
+        
+        _dft = fftshift(dft(self.processed_img, flags=cv.DFT_COMPLEX_OUTPUT))
+        self.filtered_img = _dft.copy()
+        self.filtered_img[:,:, 0] = _dft[:,:, 0] * filter
+        self.filtered_img[:,:, 1] = _dft[:,:, 1] * filter
+        self.filtered_img = idft(ifftshift(self.filtered_img), flags=cv.DFT_SCALE)
+        self.filtered_img = cv.magnitude(self.filtered_img[:,:, 0], self.filtered_img[:,:, 1])
+        self.filtered_img[self.filtered_img > 255] = 255
+        self.filtered_img[self.filtered_img < 0] = 0
+
+
+    def _GP(self, mode):
+        if mode == 'high':
+            filter = zeros(shape=self.processed_img.shape)
+            for i in range(filter.shape[0]):
+                for j in range(filter.shape[1]):
+                    d = ((i - filter.shape[0] / 2)** 2 + (j - filter.shape[1] / 2)** 2)** .5
+                    filter[i][j] = 1 - math.exp(-d ** 2 / 2 / self.cutoff_box.value() / self.cutoff_box.value())
+        
+        if mode == 'low':
+            filter = zeros(shape=self.processed_img.shape)
+            for i in range(filter.shape[0]):
+                for j in range(filter.shape[1]):
+                    d = ((i - filter.shape[0] / 2)** 2 + (j - filter.shape[1] / 2)** 2)** .5
+                    filter[i][j] = math.exp(-d ** 2 / 2 / self.cutoff_box.value() / self.cutoff_box.value())
+        
+        _dft = fftshift(dft(self.processed_img, flags=cv.DFT_COMPLEX_OUTPUT))
+        self.filtered_img = _dft.copy()
+        self.filtered_img[:,:, 0] = _dft[:,:, 0] * filter
+        self.filtered_img[:,:, 1] = _dft[:,:, 1] * filter
+        self.filtered_img = idft(ifftshift(self.filtered_img), flags=cv.DFT_SCALE)
+        self.filtered_img = cv.magnitude(self.filtered_img[:,:, 0], self.filtered_img[:,:, 1])
+        self.filtered_img[self.filtered_img > 255] = 255
+        self.filtered_img[self.filtered_img < 0] = 0
+    
+    def _HOMO(self):
         filter = zeros(shape=self.processed_img.shape)
         for i in range(filter.shape[0]):
             for j in range(filter.shape[1]):
-                if ((i - filter.shape[0] / 2)** 2 + (j - filter.shape[1] / 2)** 2)** .5 <= self.cutoff_box.value():
-                    filter[i][j] = 1
-
-        self.filtered_img[:, 0] = fft2(self.processed_img)[:, 0] * filter
-        self.filtered_img[:, 1] = fft2(self.processed_img)[:, 1] * filter
-
-
-    # def process(self):
-    #     start_time = time()
+                d = ((i - filter.shape[0] / 2)** 2 + (j - filter.shape[1] / 2)** 2)** .5
+                filter[i][j] = (self.rH_box.value() - self.rL_box.value()) * (1 - math.exp(-d ** 2 / self.cutoff_box.value() / self.cutoff_box.value())) + self.rL_box.value()
         
-    #     btnId = self.buttonGroup.checkedId()
-
-    #     if btnId == -7:
-    #         print('conv')
-    #         self._conv()
-    #     elif btnId == -2:
-    #         print('median')
-    #         self._median()
-    #     elif btnId == -3:
-    #         print('max')
-    #         self._max()
-    #     elif btnId == -4:
-    #         print('min')
-    #         self._min()
-    #     elif btnId == -5:
-    #         print('gaussian')
-    #         self._gaussian()
-    #     elif btnId == -6:
-    #         print('sobel')
-    #         self._sobel()
-    #     elif btnId == -8:
-    #         print('LoG')
-    #         self._LoG()
-        
+        _dft = fftshift(dft(self.processed_img, flags=cv.DFT_COMPLEX_OUTPUT))
+        self.filtered_img = _dft.copy()
+        self.filtered_img[:,:, 0] = _dft[:,:, 0] * filter
+        self.filtered_img[:,:, 1] = _dft[:,:, 1] * filter
+        self.filtered_img = idft(ifftshift(self.filtered_img), flags=cv.DFT_SCALE)
+        self.filtered_img = cv.magnitude(self.filtered_img[:,:, 0], self.filtered_img[:,:, 1])
+        self.filtered_img[self.filtered_img > 255] = 255
+        self.filtered_img[self.filtered_img < 0] = 0
     
+    def _MB(self):
+
+        filter = zeros(shape=self.processed_img.shape)
+        for i in range(filter.shape[0]):
+            for j in range(filter.shape[1]):
+                ab = (0.1 * i + 0.1 * j)
+                if i == 0 and j == 0:
+                    filter[i][j] = 0
+                    continue
+                filter[i][j] = (1 / np.pi / ab) * np.sin(np.pi * ab) * np.exp(-1j * np.pi * ab)
+
+        _dft = fftshift(dft(self.processed_img, flags=cv.DFT_COMPLEX_OUTPUT))
+        self.filtered_img = _dft.copy()
+        # self.filtered_img = _dft * filter
+        self.filtered_img[:,:, 0] = _dft[:,:, 0] * filter
+        self.filtered_img[:,:, 1] = _dft[:,:, 1] * filter
+        self.filtered_img = idft(ifftshift(self.filtered_img), flags=cv.DFT_SCALE)
+        self.filtered_img = cv.magnitude(self.filtered_img[:,:, 0], self.filtered_img[:,:, 1])
+        self.filtered_img[self.filtered_img > 255] = 255
+        self.filtered_img[self.filtered_img < 0] = 0
+
+    def _MBN(self):
+
+        pass
+
 app = QApplication(argv)
 
 window = MainWindow()
